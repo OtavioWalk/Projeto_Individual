30d7f89dc58e862773aaf593b56e2ad029632bc5
OtavioWalk <otavio.carvalho@sptech.school>
2023-06-10 21:37:56 -0300

    Ajustes finais no html/css

--- site/src/database/script-tabelas.sql
+++ site/src/database/script-tabelas.sql
@@ -7,8 +7,8 @@ comandos para mysql - banco local - ambiente de desenvolvimento
 */
 
 CREATE DATABASE psblog;
-
 USE psblog;
+
 
 CREATE TABLE usuario (
 	id INT PRIMARY KEY AUTO_INCREMENT,
@@ -17,32 +17,95 @@ CREATE TABLE usuario (
 	senha VARCHAR(50)
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+insert into usuario values (null, 'Marileny','marileny@gmail.com', 54321),
+						   (null, 'Carlos','carlos@gmail.com', 54321),
+						   (null, 'Arthur','arthur@gmail.com', 12345),
+						   (null, 'Otávio', 'otavio@gmail.com', 12345);
+
+select * from usuario;
+
+
+CREATE TABLE Telefone (
+	idtelefone int primary key auto_increment,
+	fkUsuario int,
+    telefone varchar(45),
+    foreign key (fkUsuario) references usuario(id)
 );
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300)
+select* from telefone;
+
+
+create table Resultado (
+	idResultado int primary key auto_increment,
+	fkUsuario int,
+    Resultado varchar(45),
+    foreign key (fkUsuario) references usuario(id)
 );
 
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
+insert into Resultado values (null,1,'Aloy'),
+							  (null,2,'Kratos'),
+							  (null,3,'Deacon'),
+							  (null,4,'Jim Sakai'),
+                              (null,1,'Aloy'),
+							  (null,2,'Kratos'),
+							  (null,3,'Deacon'),
+							  (null,4,'Jim Sakai'),
+                              (null,1,'Aloy'),
+							  (null,2,'Kratos');
 
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
+select * from Resultado;
+
+
+SELECT COUNT(*) AS Quantidade FROM Resultado
+WHERE Resultado IN ('Aloy', 'Kratos', 'Deacon', 'Jim Sakai')
+GROUP BY Resultado;
+
+
+
+
+
+drop table Resultado;
+drop table usuario;
+drop table telefone;
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- CREATE TABLE aviso (
+-- 	id INT PRIMARY KEY AUTO_INCREMENT,
+-- 	titulo VARCHAR(100),
+-- 	descricao VARCHAR(150),
+-- 	fk_usuario INT,
+-- 	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+-- );
+
+-- create table aquario (
+-- /* em nossa regra de negócio, um aquario tem apenas um sensor */
+-- 	id INT PRIMARY KEY AUTO_INCREMENT,
+-- 	descricao VARCHAR(300)
+-- );
+
+-- /* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
+
+-- create table medida (
+-- 	id INT PRIMARY KEY AUTO_INCREMENT,
+-- 	dht11_umidade DECIMAL,
+-- 	dht11_temperatura DECIMAL,
+-- 	luminosidade DECIMAL,
+-- 	lm35_temperatura DECIMAL,
+-- 	chave TINYINT,
+-- 	momento DATETIME,
+-- 	fk_aquario INT,
+-- 	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
+-- );
 
 
